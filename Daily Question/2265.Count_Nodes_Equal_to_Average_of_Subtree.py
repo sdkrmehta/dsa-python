171146ab0100df33da3d8844ef3c3436d457941a
Suraj <surajdeepkumarmehta@gmail.com>
2026-09-10 21:44:44 +0530

    add daily problem solution

---
+++ Daily Question/2265.Count_Nodes_Equal_to_Average_of_Subtree.py
@@ -0,0 +1,23 @@
+def __init__(self):
+        self.ans = 0
+
+def averageOfSubtree(self, root):
+    self.dfs(root)
+    return self.ans
+
+def dfs(self, node):
+    if node is None:
+        return [0, 0]
+
+    left = self.dfs(node.left)
+    right = self.dfs(node.right)
+
+    total_sum = left[0] + right[0] + node.val
+    count = left[1] + right[1] + 1
+
+    if node.val == total_sum // count:
+        self.ans += 1
+
+    return [total_sum, count]
+
+print(averageOfSubtree(root = [4,8,5,0,1,null,6]))
