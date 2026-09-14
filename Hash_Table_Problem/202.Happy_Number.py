a72f33bf4a3dab6982847393843a3a6f7a396dd7
Suraj <surajdeepkumarmehta@gmail.com>
2026-09-14 09:29:10 +0530

    add hash table problem solution

---
+++ Hash_Table_Problem/202.Happy_Number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        ans = set()
+
+        while n not in ans:
+            ans.add(n)
+            n = self.sumOfSquares(n)
+
+            if n == 1:
+                return True
+
+        return False
+
+    def sumOfSquares(self, n: int) -> int:
+        output = 0
+
+        while n:
+            digit = n % 10
+            digit **= 2
+            output += digit
+            n //= 10
+            
+        return output
